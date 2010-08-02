19ddfccb1239ad80cc63607af0e49ab31403184f
marceloverdijk <marceloverdijk@eb094156-6a93-46f2-7163-e0a03cb7e90b>
2010-08-02 20:53:27 +0000

    work in progress on activity class

--- janrain4j-core/src/main/java/com/googlecode/janrain4j/api/engage/Activity.java
+++ janrain4j-core/src/main/java/com/googlecode/janrain4j/api/engage/Activity.java
@@ -14,12 +14,110 @@
  */
 package com.googlecode.janrain4j.api.engage;
 
+import java.net.URL;
+
 /**
- * TODO
+ * Represents an activity update to be posted to the user's activity stream.  
  * 
  * @author Marcel Overdijk
  * @since 1.0
  */
 public class Activity {
 
+    private URL url;
+    private String action;
+    private String userGeneratedContent;
+    private String title;
+    private String description;
+    // TODO private ? actionLinks
+    // TODO private ? media
+    // TODO private ? properties
+    
+    public Activity(URL url, String action) {
+        this.url = url;
+        this.action = action;
+    }
+    
+    /**
+     * Returns the url of the resource being mentioned in the activity update
+     */
+    public URL getUrl() {
+        return url;
+    }
+    
+    /**
+     * Sets the url of the resource being mentioned in the activity update
+     * @param url The url.
+     */
+    public void setUrl(URL url) {
+        this.url = url;
+    }
+    
+    /**
+     * Returns the string describing what the user did.
+     */
+    public String getAction() {
+        return action;
+    }
+    
+    /**
+     * Sets the string describing what the user did, written in the third person. 
+     * Examples:
+     * <ul>
+     *   <li>"wrote a restaurant review"</li>
+     *   <li>"posted a comment"</li>
+     *   <li>"took a quiz"</li>
+     * </ul>
+     * @param action The string describing what the user did.
+     */
+    public void setAction(String action) {
+        this.action = action;
+    }
+    
+    /**
+     * Returns the string containing the user-supplied content.
+     */
+    public String getUserGeneratedContent() {
+        return userGeneratedContent;
+    }
+    
+    /**
+     * Sets the string containing the user-supplied content, such as a comment 
+     * or the first paragraph of an article that the user wrote. Note that some 
+     * providers (Twitter in particular) may truncate this value.
+     * @param userGeneratedContent The string containing the user-supplied content.
+     */
+    public void setUserGeneratedContent(String userGeneratedContent) {
+        this.userGeneratedContent = userGeneratedContent;
+    }
+    
+    /**
+     * Returns the title of the resource being mentioned in the activity update.
+     */
+    public String getTitle() {
+        return title;
+    }
+    
+    /**
+     * Sets the title of the resource being mentioned in the activity update.
+     * @param title The title of the resource being mentioned in the activity update.
+     */
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    
+    /**
+     * Returns the description of the resource mentioned in the activity update.
+     */
+    public String getDescription() {
+        return description;
+    }
+    
+    /**
+     * Sets the description of the resource mentioned in the activity update.
+     * @param description The description of the resource mentioned in the activity update.
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
