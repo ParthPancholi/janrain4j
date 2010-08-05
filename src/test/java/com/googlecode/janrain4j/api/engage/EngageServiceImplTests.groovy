fe2e7bfd8dd6cadbc6016b9764c571dc62f2e9d8
marceloverdijk <marceloverdijk@eb094156-6a93-46f2-7163-e0a03cb7e90b>
2010-08-05 21:09:54 +0000

    hell yeah! added groovy nature and first test

---
+++ src/test/java/com/googlecode/janrain4j/api/engage/EngageServiceImplTests.groovy
@@ -0,0 +1,13 @@
+package com.googlecode.janrain4j.api.engage
+
+import static org.junit.Assert.*
+
+import org.junit.Test
+
+class EngageServiceImplTests {
+
+    @Test
+    public void testMappings() {
+        fail "Not yet implemented"
+    }
+}
